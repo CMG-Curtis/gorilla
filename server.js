62a84e93efb60413925056c1f5e9cad770482d54
CMG_Curtis <curtis@checkmategaming.com>
2017-07-08 02:37:41 -0400

    removed unnecessary things and added some comments

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
+// Dependencies 
 var MongoClient = require('mongodb').MongoClient;
 var stringify = require('json-stringify');
-
 var express = require('express');
 var app = express();
+var port = 3000;
 
+// Login information for server
 var dbUsername = 'cmguser';
 var dbPassword = 'cmgpass';
 
+// Database information
 var DATABASE = 'gorilla';
-
 var DATABASE_URL = 'mongodb://' + dbUsername + ':' + dbPassword + '@localhost:27017/' + DATABASE;
 
-var USER_TABLE = 'users';
-var TOURNAMENTS_TABLE = 'tournaments';
+// Collections on the databse
+var USERS_COLLECTION = 'users';
+var TOURNAMENTS_COLLECTION = 'tournaments';
 
-var port = 3000;
+// Page for viewing raw collections from the database
+app.get('/db/' + USERS_COLLECTION, (req, res) => {
+	var collection = req.path.split('/');
+	collection = collection[collection.length - 1];
+	getEntriesForHTML(USERS_COLLECTION, req, res);
+});
 
-// Page for viewing raw users
-app.get('/db/' + USER_TABLE, (req, res) => {
+// Get all entries for the given collection, stringify them, and add <br>s between them
+function getEntriesForHTML(collection, req, res){
 	MongoClient.connect(DATABASE_URL, function(err, db) {
 		if (err) throw err;
-		var users = db.collection(USER_TABLE);
-		var allUsers = users.find({}).toArray(function(err, items) {
+		var users = db.collection(collection);
+		var allUsers = users.find({}, {_id: 0}).toArray(function(err, items) {
 			if (err) throw err;
 			var responseText = '';
 			for(item of items){
@@ -36,51 +44,22 @@ app.get('/db/' + USER_TABLE, (req, res) => {
 			db.close();
 		});
 	});
-});
+}
 
-// Page for viewing raw tournaments
-app.get('/db/' + TOURNAMENTS_TABLE, (req, res) => {
+// Remove all entries from the given collection
+function deleteAllEntries(collection){
 	MongoClient.connect(DATABASE_URL, function(err, db) {
 		if (err) throw err;
-		var tournaments = db.collection(TOURNAMENTS_TABLE);
-		var allTournaments = tournaments.find({}, { _id: false }).toArray(function(err, items) {
-			if (err) throw err;
-			var responseText = '';
-			for(item of items){
-				responseText += stringify(item);
-				responseText += '<br>';
-			}
-			if(items.length == 0){
-				res.send('No entries');
-			} else {
-				res.send(responseText);
-			}
-			db.close();
-		});
+		var collection = db.collection(collection);
+		collection.deleteMany();
+		db.close();
 	});
-});
+}
 
-
-MongoClient.connect(DATABASE_URL, function(err, db) {
-	if (err) throw err;
-	var users = db.collection(USER_TABLE);
-	/*
-	// Delete all users
-	users.deleteMany();
-	// Insert some random data for testing
-	for(var i = 0; i < 20; i++){
-		users.insertOne({number:Math.random()}, (err, res) => {
-			if(err) throw err;
-			console.log('inserted something');
-		});
-	}
-	*/
-	db.close();
-});
-
-
+// Setup static routes for webserver
 app.use(express.static('public'));
 
+// Start webserver on given port
 app.listen(port, () => {
 	console.log('Webserver listening on ' + port);
 });
