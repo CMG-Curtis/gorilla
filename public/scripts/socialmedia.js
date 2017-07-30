c751be95dda1b94fc4b31a689b8d44bdd2843c6f
CMG_Curtis <curtis@checkmategaming.com>
2017-07-29 20:15:26 -0400

    twitter farming is now working in a very basic form

--- public/scripts/socialmedia.js
+++ public/scripts/socialmedia.js
@@ -5,18 +5,44 @@ var Twitter = {};
 // Passes back the amount of followers from a given twitter page
 Twitter.getFollowers = function(url, callback){
 	getPage(url, function(html){
-		var data = JSON.parse(html.querySelector('input.json-data').value);
-		callback(data.profile_user.followers_count);
+		var element = html.querySelector('input.json-data');
+		if(!element){
+			callback(-1);
+			return;
+		}
+		var data = JSON.parse(element.value);
+		if(data){
+			if(!data.profile_user){
+				callback(-1);
+				return;
+			}
+			callback(data.profile_user.followers_count);
+		} else {
+			callback(-1);
+		}
 	});
 };
 
-// Tesing of twitter getFollowers stuff
-Database.login('cmguser', 'cmgpass');
+var twitterQuery = document.getElementById('twitter');
+var twitterQueryFilter = document.getElementById('twitterFollwersFilter');
 
-Database.getDocuments('users', { "twitter" : { "$exists" : true } }, function(res){
-	console.log(res.documents[0].twitter);
-	Twitter.getFollowers(res.documents[0].twitter);
-	for(doc of res.documents){
-		//console.log(doc.twitter);
-	}
+var results = document.getElementById('results');
+
+twitterQuery.addEventListener('click', function(){
+	results.innerHTML = '';
+	Database.getDocuments('users', { "twitter" : { "$exists" : true } }, function(res){
+		var len = res.documents.length;
+		res.documents.forEach(function(listItem, index){
+			Twitter.getFollowers(listItem.twitter, function(followers){
+				if(followers > twitterQueryFilter.value){
+					var splitting = listItem.twitter.split('/');
+					var handle = splitting[splitting.length - 1];
+					results.innerHTML += '<a target="_blank" href="' + listItem.twitter + '">' + followers  + ' : ' + handle + '</a>';
+				}
+				if(index == len - 1){
+					results.innerHTML += '<p>Done.</p>'
+				}
+			});
+		});
+	});
 });
